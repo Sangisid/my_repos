77de76f4a2174212920cff49faf1966d73f6a803
Sangisid <54971081+Sangisid@users.noreply.github.com>
2019-09-06 16:36:08 +0800

    Add files via upload

---
+++ Github_nb.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This Notebook will be mainly used for Capstone Project
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[1]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
